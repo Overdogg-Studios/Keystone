0176261cc6dcff28c761c06a0759b24b1dc95f84
Craig Ferris <cmferris97@yahoo.com>
2016-12-26 19:22:34 -0500

    Added Two Prototype Enemies
    -First enemy is a motion sensor
    When player gets close to motion sensor it activates a laser system
    attached to the sensor
    The lasers turn on and off from then on
    -The second enemy is an arrow launching enemy.
    When the player gets close it fires arrows at the player

---
+++ Assets/Scripts/Enemies-Sensors/LaserSensor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Enemies_Sensors
+{
+    class LaserSensor : MonoBehaviour
+    {
+        public int damage;
+        public bool isEnemy;
+        private PlayerController allen;
+
+        void Start()
+        {
+        }
+
+        void OnTriggerEnter2D(Collider2D collision)
+        {
+            allen = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+            if(collision.gameObject.tag == "Player" && isEnemy)
+            {
+                Laser[] l = GetComponentsInChildren<Laser>();
+                for(int i = 0; i < l.Length; i++)
+                {
+                    l[i].damage = damage;
+                    l[i].isActivated = true;
+                }
+            }
+        }
+    }
+}
