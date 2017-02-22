07d63d361124bfb1f49513c05db16ceba568c451
RobertMisch <robertmisch94@gmail.com>
2017-02-21 21:58:03 -0500

    First start on dialogue
    Just a basic textbox so far

---
+++ Assets/Scripts/Engine/DialogueImporter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueImporter : MonoBehaviour {
+
+	public TextAsset textFile;
+	public string[] textLines;
+
+
+	// Use this for initialization
+	void Start () {
+		
+		if(textFile != null)
+		{
+			textLines = (textFile.text.Split('\n'));
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Engine/DialogueManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+	public GameObject textBox;
+
+	public Text theText;
+
+	public TextAsset textFile;
+	public string[] textLines;
+
+	public int currentLine;
+	public int endAtLine;
+
+	public PlayerController player;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		player = FindObjectOfType<PlayerController>();
+
+		if(textFile != null)
+		{
+			textLines = (textFile.text.Split('\n'));
+		}
+
+		if(endAtLine == 0)
+		{
+			endAtLine = textLines.Length - 1;
+		}
+	}
+
+	void Update()
+	{
+
+		theText.text = textLines[currentLine];
+
+		if(Input.GetKeyDown(KeyCode.Return))
+		{
+			currentLine += 1;
+		}
+
+		if(currentLine > endAtLine)
+		{
+			textBox.SetActive(false);
+		}
+	}
+}
