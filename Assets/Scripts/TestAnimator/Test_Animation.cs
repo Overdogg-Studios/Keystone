28bdbb8a8c42f63365cb98bbb3bac6479fea6153
Craig Ferris <cmferris97@yahoo.com>
2017-02-27 14:22:38 -0500

    Added  Animator

--- Assets/Scripts/TestAnimator/Test_Animation.cs
+++ Assets/Scripts/TestAnimator/Test_Animation.cs
@@ -2,28 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test_Animation : MonoBehaviour {
+public class Test_Animation {
 
     private Sprite[] sprites;
     public Texture2D texture;
     public float timeToDisplayFrame;
+    public bool faceLeft;
+    public bool animate;
 
     private SpriteRenderer renderer;
     private int frameCounter;
     private float timer = 0.0f;
 
+    public Test_Animation(Texture2D texture, float timeToDisplayFrame, bool faceLeft, bool animate, SpriteRenderer renderer)
+    {
+        this.texture = texture;
+        this.timeToDisplayFrame = timeToDisplayFrame;
+        this.faceLeft = faceLeft;
+        this.animate = animate;
 
+
+        sprites = Resources.LoadAll<Sprite>(texture.name);
+        //Debug.Log(sprites.Length);
+        frameCounter = 0;
+        //renderer = GetComponent<SpriteRenderer>();
+        this.renderer = renderer;
+    }
 
 	// Use this for initialization
 	void Start ()
     {
-        sprites = Resources.LoadAll<Sprite>(texture.name);
-        frameCounter = 0;
-        renderer = GetComponent<SpriteRenderer>();
+        //sprites = Resources.LoadAll<Sprite>(texture.name);
+        //frameCounter = 0;
+        //renderer = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate ()
+	public void FixedUpdate ()
     {
         timer += Time.deltaTime;
         if(timer >= timeToDisplayFrame)
@@ -35,7 +50,25 @@ public class Test_Animation : MonoBehaviour {
             }
             timer = 0.0f;
         }
-        Debug.Log(frameCounter);
+        //Debug.Log(frameCounter);
         renderer.sprite = sprites[frameCounter];
+        if(faceLeft)
+        {
+            renderer.flipX = true;
+        }
+        else
+        {
+            renderer.flipX = false;
+        }
 	}
+
+    public void setAnimate(bool animate)
+    {
+        this.animate = animate;
+    }
+
+    public void setFacingLeft(bool left)
+    {
+        this.faceLeft = left;
+    }
 }
---
+++ Assets/Scripts/TestAnimator/Test_Animator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Animator : MonoBehaviour {
+
+    public Texture2D sprint;
+    public Texture2D walk;
+
+    private Test_Animation sprintAnim;
+    private Test_Animation walkAnim;
+
+
+    private float testTimer = 0.0f; // Used so i do not have to press keys for testing
+
+	// Use this for initialization
+	void Start ()
+    {
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        sprintAnim = new Test_Animation(sprint, 1, false,false,renderer);
+        walkAnim = new Test_Animation(walk, 1, false,true,renderer);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        if(testTimer > 10)
+        {
+            walkAnim.setAnimate(false);
+            sprintAnim.setAnimate(true);
+            sprintAnim.FixedUpdate();
+        }
+        else if(testTimer > 5)
+        {
+            walkAnim.setFacingLeft(true);
+            walkAnim.FixedUpdate();
+        }
+        else
+        {
+            walkAnim.FixedUpdate();
+        }
+        testTimer += Time.deltaTime;
+	}
+}
