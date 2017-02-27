a3901de57f69b7f0533a7283d5a1044cced06b42
Craig Ferris <cmferris97@yahoo.com>
2017-02-27 13:50:57 -0500

    Added Animations

---
+++ Assets/Scripts/TestAnimator/Test_Animation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Animation : MonoBehaviour {
+
+    private Sprite[] sprites;
+    public Texture2D texture;
+    public float timeToDisplayFrame;
+
+    private SpriteRenderer renderer;
+    private int frameCounter;
+    private float timer = 0.0f;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        sprites = Resources.LoadAll<Sprite>(texture.name);
+        frameCounter = 0;
+        renderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        timer += Time.deltaTime;
+        if(timer >= timeToDisplayFrame)
+        {
+            frameCounter++;
+            if (frameCounter >= sprites.Length)
+            {
+                frameCounter = 0;
+            }
+            timer = 0.0f;
+        }
+        Debug.Log(frameCounter);
+        renderer.sprite = sprites[frameCounter];
+	}
+}
