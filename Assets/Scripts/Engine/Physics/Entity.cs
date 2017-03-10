48de0012454c29dd0c94ff342b821f300ffaa5a0
Sean Carpenter <seancarpenter10@gmail.com>
2017-03-09 21:01:59 -0500

    AHAHAHAHAHAHAHA

--- Assets/Scripts/Engine/Physics/Entity.cs
+++ Assets/Scripts/Engine/Physics/Entity.cs
@@ -2,122 +2,147 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/**
- * Entity is designed to simulate gravity in the place of Rigidbody.
- * @type {[type]}
- */
 public class Entity : MonoBehaviour {
 
-	public bool grounded;
-	public float mass;
-	public float gravity;
-	public float velocityX;
-	public float velocityY;
-	public float maxVelocityY;
-	public float maxVelocityX;
-	public bool isKinematic;
-	private BoxCollider2D bottomCollider;
-	private BoxCollider2D topCollider;
-	private BoxCollider2D rightCollider;
-	private BoxCollider2D leftCollider;
-	public BoxCollider2D collider;
-	public LayerMask groundMask;
-	// Use this for initialization
-	void Start () {
+    [HideInInspector]
+    public Vector2 position;
+    [HideInInspector]
+    public Vector2 previous;
 
-		collider = GetComponent<BoxCollider2D>();
-		bottomCollider = gameObject.AddComponent<BoxCollider2D>();
-		topCollider = gameObject.AddComponent<BoxCollider2D>();
-		leftCollider = gameObject.AddComponent<BoxCollider2D>();
-		rightCollider = gameObject.AddComponent<BoxCollider2D>();
-		updateEdgeColliders();
-		groundMask = LayerMask.GetMask("Ground");
-	}
-	// Update is called once per frame
-	void FixedUpdate () {
+    public LayerMask collideWith;
+    public Rect hitBox;
 
-		updateEdgeColliders();
-		/*
-		if (!isKinematic)
-		{
-			velocityY = velocityY - (gravity * mass);
-		}
+    public Vector2 velocity;
+    public Vector2 acceleration;
 
-		
+    public bool onGround = false;
 
-		if(!isGrounded()) {
-			
-			velocityY = velocityY - (Time.deltaTime * gravity);
-			transform.position = new Vector3(transform.position.x + (velocityX), transform.position.y + (velocityY), -1); 
-		}
-		else {
-			velocityY = 0;
-			
-		}
-		Debug.Log(isGrounded());
-		
-		capVelocity();
-		transform.position = new Vector3(transform.position.x + (velocityX), transform.position.y + (velocityY), -1); 
-		*/
+    //Helper Functions
+    public Vector2 direction
+    {
+        get {
+            return velocity.normalized;
+        }
+        set {
+            velocity = value.normalized * velocity.magnitude;
+        }
+    }
+    public float speed
+    {
+        get {
+            return velocity.magnitude;
+        }
+        set {
+            velocity = direction * speed;
+        }
+    }
+
+    [ExecuteInEditMode]
+	void Start ()
+    {
+        position = transform.position;
+        previous = position;
+
 	}
 
-	public void addForceY(float force) {
-		velocityY += force;
-		
+    //Validate hitbox size in editor
+    private void OnValidate()
+    {
+        if (hitBox.size == Vector2.zero)
+            hitBox.size = Vector2.one;
+    }
+
+    private void Update()
+    {
+        PhysUpdate();
+
+        onGround = !placeFree(position + (Vector2.down * 0.1F) );
+
+        //Error Handling
+        if( !placeFree(position) )
+        {
+            Debug.Log("stuck in wall");
+        }
+    }
+
+	void PhysUpdate ()
+    {
+        position = transform.position;
+        previous = position;
+
+        Vector2 newPosition = position + velocity * Time.deltaTime;
+        velocity += acceleration * Time.deltaTime;
+
+        //Attempt move; if position blocked, slide along X or Y axis
+        if ( !moveTo( newPosition ) )
+        {
+            if ( moveTo(new Vector2(position.x, newPosition.y)))
+            {
+                moveContact(new Vector2(newPosition.x, position.y));
+                velocity.x = 0;
+            }
+            else if ( moveTo(new Vector2(newPosition.x, position.y)))
+            {
+                moveContact(new Vector2(position.x, newPosition.y));
+                velocity.y = 0;
+            }
+            else
+                moveContact(new Vector2(position.x, newPosition.y));
+
+        }
+
+        //Move Transform
+        transform.position = new Vector3(position.x, position.y, transform.position.z);
 	}
-	public void capVelocity() {
 
-		if (velocityY > maxVelocityY)
-		{
-			velocityY = maxVelocityY;
-		} else if (velocityY < -maxVelocityY)
+    bool moveTo(Vector2 newPosition)
+    {
+        if (placeFree(newPosition))
+        {
+            position = newPosition;
+            return true;
+        }
+        return false;
+    }
 
-		{
-			velocityY = -maxVelocityY;
-		}
-	}
-	void OnDrawGizmos () {
-		Gizmos.color = new Color(1F, 0F, 0F, 1F);
-		//Right Side Collider
-		
-	}
-	public void updateEdgeColliders() {
+    void moveContact(Vector2 newPosition)
+    {
+        float precision = 0.1F;
+        Vector2 movement = newPosition - position;
+        Vector2 bestPosition = position;
+        for (float i = 0; i <= 1; i += precision)
+        {
+            if ( placeFree(position + movement * i) )
+                bestPosition = position+movement*i;
+            else
+            {
+                position = bestPosition;
+                return;
+            }
+        }
+        Debug.Log("MoveContact Fail: (" + movement.x + ") , (" + movement.y + ")");
+    }
 
-		Vector2 offset = new Vector2(collider.size.x / 2 + collider.offset.x, collider.offset.y);
-		Vector2 size = new Vector2((collider.size.x)/10, (collider.size.y)/1.1f);
-		rightCollider.offset = offset;
-		rightCollider.size = size;
+    bool placeFree( Vector2 newPosition )
+    {
+        Collider2D hit = Physics2D.OverlapBox(newPosition+hitBox.position, hitBox.size, 0, collideWith);
+        return !hit;
+    }
 
-		offset = new Vector2(-collider.size.x / 2 + collider.offset.x, collider.offset.y);
-		size = new Vector2((collider.size.x)/10, (collider.size.y)/1.1f);
-		leftCollider.offset = offset;
-		leftCollider.size = size;
-
-		offset = new Vector2(collider.offset.x, collider.size.y / 2 + collider.offset.y);
-		size = new Vector2((collider.size.x)/1.1f, (collider.size.y)/10f);
-		topCollider.offset = offset;
-		topCollider.size = size;
-
-		offset = new Vector2(collider.offset.x, -collider.size.y / 2 + collider.offset.y);
-		size = new Vector2((collider.size.x)/1.1f, (collider.size.y)/10f);
-		bottomCollider.offset = offset;
-		bottomCollider.size = size;
-	}
-
-	/**
-	 * Check to see if the entity is touching the ground, ceiling or a wall on either side.
-	 * @return true if touching, false otherwise.
-	 */
-	public bool isGrounded() {
-		return Physics2D.OverlapBox(bottomCollider.bounds.center, bottomCollider.bounds.size, 0f, groundMask);
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireCube(new Vector3(transform.position.x + hitBox.x, transform.position.y + hitBox.y, transform.position.z), hitBox.size );
+    }
+    public bool isGrounded() {
+		return onGround;
 	}
 	public bool isTouchingCeiling() {
-		return Physics2D.OverlapBox(topCollider.bounds.center, topCollider.bounds.size, 0f, groundMask);
+		return false;
 	}
 	public bool isTouchingRightWall() {
-		return Physics2D.OverlapBox(rightCollider.bounds.center, rightCollider.bounds.size, 0f, groundMask);
+		return false;
 	}
 	public bool isTouchingLeftWall() {
-		return Physics2D.OverlapBox(leftCollider.bounds.center, leftCollider.bounds.size, 0f, groundMask);
+		return false;
 	}
 }
